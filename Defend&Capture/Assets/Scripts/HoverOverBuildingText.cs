405c538640ea3da7ac4525cfb9d943bd0b1d142c
Dracknid10 <55700825+Dracknid10@users.noreply.github.com>
2022-02-05 23:31:31 +0000

    soilder models imported

--- Defend&Capture/Assets/Scripts/HoverOverBuildingText.cs
+++ Defend&Capture/Assets/Scripts/HoverOverBuildingText.cs
@@ -7,6 +7,10 @@ using UnityEngine.EventSystems;
 
 public class HoverOverBuildingText : MonoBehaviour
 {
+
+    public Text Descr;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +21,17 @@ public class HoverOverBuildingText : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    public void barracksText()
+    {
+
+
+
+        Descr.text = "The Barracs is used to create infantry troops";
+
+
     }
 
 
