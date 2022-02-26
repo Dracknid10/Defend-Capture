e523cd1834c8f14f1df1be1941f6105d6b904a50
Dracknid10 <55700825+Dracknid10@users.noreply.github.com>
2022-02-26 00:32:50 +0000

    supply pad funcitonality

--- Defend&Capture/Assets/Prefabs/Supplypad1.cs
+++ Defend&Capture/Assets/Prefabs/Supplypad1.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Supplypad1 : MonoBehaviour
 {
+
+
+    private statManager manager;
+    private bool addgap = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
+        manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<statManager>();
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+        if (!addgap)
+        {
+            StartCoroutine(addSupplies());
+        }
         
+
+
     }
+
+    IEnumerator addSupplies()
+    {
+
+        addgap = true;
+
+        if (addgap)
+        {
+            manager.addsupplies(20);
+        }
+
+
+        
+        yield return new WaitForSeconds(1);
+        addgap = false;
+
+    }
+
+
 }
